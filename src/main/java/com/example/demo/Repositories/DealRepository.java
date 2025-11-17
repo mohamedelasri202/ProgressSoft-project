77134555c36a35c0f8c798a8210ce86856b165a2
mohamedelasri202 <el7994995@gmail.com>
2025-11-17 18:09:55 +0000

    adding the service  and the repository

--- src/main/java/com/example/demo/Repositories/DealRepository.java
+++ src/main/java/com/example/demo/Repositories/DealRepository.java
@@ -1,7 +1,7 @@
-//package com.example.demo.Repositories;
-//
-//import com.example.demo.Models.Deal;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface DealRepository extends JpaRepository<Deal,String> {
-//}
+package com.example.demo.Repositories;
+
+import com.example.demo.Models.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DealRepository extends JpaRepository<Deal,String> {
+}
